b406d180af7543c80b52fa33339d90df6f8e6552
rio1216 <44338992+rio1216@users.noreply.github.com>
2019-01-06 23:37:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import urllib.request
+import HttpClient
 from datetime import datetime
-
-def rawWithAgent(url):
-    info("Fetching: " + url)
-    values = {"User-Agent": "Mozilla/5.0"}
-    req = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(values)))
-    with urllib.request.urlopen(req) as res:
-        body = res.read()
-        return str(body)
 
 def info(message):
     print(getTime() + " " + message)
@@ -16,7 +8,7 @@ def getTime():
     return datetime.now().strftime("[%H:%M:%S]")
 
 def main():
-    result = rawWithAgent("https://raw.githubusercontent.com/rio1216/python-study/master/hello.txt")
+    result = HttpClient.rawWithAgent("https://raw.githubusercontent.com/rio1216/python-study/master/hello.txt")
     info("Http Result: " + result)
 
 if __name__ == "__main__":
